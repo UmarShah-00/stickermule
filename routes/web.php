6da4aa6e3cd274399beff22a197d8bdc8dc91cfa
Your Name <youremail@example.com>
2025-07-23 23:16:53 +0500

    ...

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardsController;
 
 
@@ -24,17 +22,5 @@ use App\Http\Controllers\DashboardsController;
 
 // Route::get('', [Controller::class, 'index']);
 
-// Public routes
-Route::get('/login', [UserController::class, 'showform'])->name('login');
-
-// Protected routes (only for logged-in users)
-Route::middleware(['auth'])->group(function () {
-    
-    // Dashboard
-    Route::get('/', [DashboardsController::class, 'index'])->name('dashboard');
-
-    // Profile
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// DASHBOARDS //
+Route::get('/', [DashboardsController::class, 'index']);
